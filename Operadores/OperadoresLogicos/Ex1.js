dd51a22f257afaf96251a603088f08168350e5dc
GUSTAVO DE LIMA DOS SANTOS <05079304014@senacrs.edu.br>
2026-03-26 17:25:37 -0300

    trabalhando com operadores

---
+++ Operadores/OperadoresLogicos/Ex1.js
@@ -0,0 +1,59 @@
+/*Operador E ( tudo deve ser verdadeiro ) .
+Representamos com : &&
+
+-Operador ou (Uma condiçao sendo verdadeira ja basta).
+ Representamos com :||
+
+-Operador Nao (Inverte o valor)
+Representamos com : !
+
+*/
+
+/*
+
+const idade = 18
+const temAssinatura = true
+
+const podePassar = idade >= 18 && temAssinatura
+
+console.log(podePassar)
+
+*/
+
+ /*
+const idade = 19
+const identidade = true
+
+const podeBeber = idade >= 18 && identidade
+
+console.log(podeBeber)
+
+ */
+
+/*
+const valorCompra = 200
+const clienteVip = false
+const podeUsarcupom = valorCompra >100 ||clienteVip
+console.log(podeUsarcupom)
+
+*/
+
+/*
+const idade = 50
+const deficiencia = true
+const podeEstacionar = idade >= 60 || deficiencia
+
+console.log(podeEstacionar)
+*/
+
+const nf = 6
+const nr = 6
+const t1 = 6
+const t2 = 5
+const t3 = 7
+
+const questao2 = nf >= 8 || nr >= 8 && t1 >=6 && t2 >=6
+
+
+console.log (questao2)
+
